37ee0089c2780be805af7aa083170a3afe445aab
Goh Yong Jie <77619474+gohyongjie1112@users.noreply.github.com>
2022-06-25 14:04:04 +0800

    Init commit

---
+++ BipBillyTennis/README.md
@@ -0,0 +1,3 @@
+# BipBillyTennis
+
+Testing
---
+++ BipBillyTennis/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $("#fees-btn").click(function () { 
+        location.href = 'http://127.0.0.1:5500/pages/lessons_and_venues.html';
+    });
+});
